a02d9ebe76253378b1741d3dee10511327229704
ZubairAslamNow <zubair.aslam@now.net.pk>
2025-10-16 11:48:22 +0500

    Initial commit

---
+++ test_github_action/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test_github_action
+//
+//  Created by NowSoft on 16/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test_github_action/test_github_actionApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_github_actionApp.swift
+//  test_github_action
+//
+//  Created by NowSoft on 16/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct test_github_actionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
